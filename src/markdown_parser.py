7b8598e61b5a28e1bd89b2f567953568c4c7ac17
Sherezz Grant <rezziemaven@gmail.com>
2026-05-09 16:07:28 -0400

    Add split_nodes_delimiter function split_nodes_delimiter is a function that parses markdown and splits up text based markdown based on the delimiter type eg. ** for bold text, _ for italic text. It returns a list of new TextNodes split up by type.

---
+++ src/markdown_parser.py
@@ -0,0 +1,22 @@
+import re
+
+from textnode import TextNode, TextType
+
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+        if node.text.count(delimiter) % 2 != 0:
+            raise Exception("Invalid Markdown syntax")
+        split_text = node.text.split(delimiter)
+        for text in split_text:
+            if len(text) == 0:
+                continue
+            if text.startswith(" ") or text.endswith(" "):
+                new_nodes.append(TextNode(text, TextType.TEXT))
+            else:
+                new_nodes.append(TextNode(text, text_type))
+    return new_nodes
