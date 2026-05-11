3891201456fb3297901c8fa0e5b57e0b94ebeb4a
Sherezz Grant <rezziemaven@gmail.com>
2026-05-11 19:50:01 -0400

    Add test_extract_markdown_images This function extracts the alt text and link from a markdown image.

--- src/markdown_parser.py
+++ src/markdown_parser.py
@@ -20,3 +20,11 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
             else:
                 new_nodes.append(TextNode(text, text_type))
     return new_nodes
+
+
+def extract_markdown_images(text):
+    matches = re.findall(r"!\[(.*?)\]\((.*?)\)", text)
+    if len(matches) == 0:
+        return []
+    return matches
+
