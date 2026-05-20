241ba700227cb0c9bdb05d92447d0329accfdc1a
Sherezz Grant <rezziemaven@gmail.com>
2026-05-20 18:37:37 -0400

    Add functionality to split nodes by link type

--- src/markdown_parser.py
+++ src/markdown_parser.py
@@ -72,3 +72,21 @@ def split_nodes_image(old_nodes):
             else:
                 new_nodes.append(TextNode(matches[0][0], TextType.IMAGE, matches[0][1]))
     return new_nodes
+
+
+def split_nodes_link(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+        split_text = re.split(r"(?<!!)(\[.*?\]\(.*?\))", node.text)
+        for text in split_text:
+            if len(text) == 0:
+                continue
+            matches = extract_markdown_links(text)
+            if len(matches) == 0:
+                new_nodes.append(TextNode(text, TextType.TEXT))
+            else:
+                new_nodes.append(TextNode(matches[0][0], TextType.LINK, matches[0][1]))
+    return new_nodes
