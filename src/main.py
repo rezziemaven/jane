765ac00ce0f96de4ae6843655ccd1c90b2f66750
Sherezz Grant <rezziemaven@gmail.com>
2026-05-27 20:34:24 -0400

    Add function to copy contents from a source directory to a destination

--- src/main.py
+++ src/main.py
@@ -1,9 +1,41 @@
+import os
+import shutil
+
 from textnode import TextNode, TextType
 
 
 def main():
-    tn = TextNode("Hello World!", TextType.TEXT, "https://google.com")
-    print(tn)
+    copy_contents("./static", "./public")
+
+def copy_contents(src, dst):
+    # check that each path exists
+    if not os.path.exists(src):
+        raise Exception("❌ Source directory not found.")
+    if not os.path.exists(dst):
+        os.mkdir(dst)
+
+    # delete public contents first
+    shutil.rmtree(dst)
+    os.mkdir(dst)
+    print("🗑️ Contents in destination directory deleted.")
+    print(f"⚙️ Copying contents from {src} to {dst} ...")
+
+    def copy_deep(src, dst):
+        items = os.listdir(src)
+        # print(items)
+
+        for item in items:
+            src_path = os.path.join(src, item)
+            if os.path.isfile(src_path):
+                dst_path = shutil.copy(src_path, dst)
+                print(f"Copied file from {src_path} to {dst_path}")
+            else:
+                dst_path = os.path.join(dst, item)
+                os.mkdir(dst_path)
+                copy_deep(src_path, dst_path)
+
+    copy_deep(src, dst)
+    print("✅ Copy complete.")
 
 
 main()
